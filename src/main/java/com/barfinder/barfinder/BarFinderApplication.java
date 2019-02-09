a99415b880b99513aca8eacb44040eb46a4bcd77
Christopher Manning <manningc13@gmail.com>
2019-02-09 10:27:18 -0500

    Added Gooooooooogle Maps dependencies; API Tolkein

--- src/main/java/com/barfinder/barfinder/BarFinderApplication.java
+++ src/main/java/com/barfinder/barfinder/BarFinderApplication.java
@@ -2,12 +2,13 @@ package com.barfinder.barfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BarFinderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BarFinderApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(BarFinderApplication.class, args);
 	}
 
 }
