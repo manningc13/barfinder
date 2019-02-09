55efbd7e992c0d59da91583695a6adbca7ffcbc2
Christopher Manning <manningc13@gmail.com>
2019-02-08 23:42:00 -0500

    Added in setup for SpringBoot application, prior to the awesome webpages

---
+++ src/main/java/com/barfinder/barfinder/BarFinderApplication.java
@@ -0,0 +1,14 @@
+package com.barfinder.barfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarFinderApplication.class, args);
+	}
+
+}
+
